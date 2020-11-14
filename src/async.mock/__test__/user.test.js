0e66b2d1a75a58be44f0593ca48020717b6d2963
SUCHMOKUO <suchmokuo@gmail.com>
2020-11-15 01:06:37 +0800

    [liuxing] finish all tests

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,28 @@
+import axios from "axios";
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    const username = "mokuo";
+    const password = "123";
+    expect(await register(username, password)).toEqual({});
+    expect(axios.post).toHaveBeenCalledWith("/user", {
+      username,
+      password,
+    });
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockImplementation(() => false);
+
+    const username = "mokuo";
+    const password = "123";
+    await expect(() => register(username, password)).rejects.toThrow();
   });
 });
